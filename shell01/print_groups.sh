38d7c79840bb4f7bfbee29b346ae0142df8b888e
daisanam <daine.sanalva@gmail.com>
2025-07-10 00:38:02 +0200

    Delete duplicated print_groups.sh outside current folder

--- shell01/print_groups.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Get the groups of the user FT_USER separated by spaces
-groups_list=$(id -Gn "$FT_USER")
-# Replace spaces with commas without spaces
-echo "$groups_list" | tr ' ' ','
-
